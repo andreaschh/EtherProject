260ebc1c523add6662742183aa22f5dd6754f017
Andreas Christodoulou <57558807+andreaschh@users.noreply.github.com>
2021-04-06 23:23:31 +0300

    TEsting

--- app/src/main/java/com/example/etherproject/Dashboard.java
+++ app/src/main/java/com/example/etherproject/Dashboard.java
@@ -19,6 +19,8 @@ public class Dashboard extends AppCompatActivity {
         getSupportActionBar().setBackgroundDrawable(cd);
     }
 
+    //  TEST
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
